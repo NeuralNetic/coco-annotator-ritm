2b0aa7dff5abc9fef3700a0550545752b3b350a4
Alexey <alexey.s.russ@gmail.com>
2023-07-25 18:06:42 +0300

    Ad script to apply exif

---
+++ scripts/apply_exif_to_images.py
@@ -0,0 +1,36 @@
+from argparse import ArgumentParser, Namespace
+import numpy as np
+import os
+from PIL import Image, ImageOps
+from tqdm import tqdm
+
+
+def parse_args() -> Namespace:
+    parser = ArgumentParser(
+        description='Apply EXIF transforms to images in a database folder'
+    )
+    parser.add_argument(
+        '-i', '--input', required=True, type=str,
+        help='Path to folder with images'
+    )
+    return parser.parse_args()
+
+
+def main():
+    args = parse_args()
+
+    input_folder = args.input
+
+    for file_name in tqdm(os.listdir(input_folder)):
+        file_path = os.path.join(input_folder, file_name)
+        if not os.path.isfile(file_path):
+            continue
+
+        pil_img = Image.open(file_path)
+        pil_img = ImageOps.exif_transpose(pil_img)
+        pil_img = Image.fromarray(np.array(pil_img.convert('RGB')))
+        pil_img.save(file_path)
+
+
+if __name__ == '__main__':
+    main()
